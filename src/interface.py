a27301f75eb223c7d946b7721770064e783f9cbd
Charllys Fernandes <charllys.daiany@gmail.com>
2024-01-31 18:30:04 -0300

    update code

--- src/interface.py
+++ src/interface.py
@@ -11,25 +11,25 @@ class Interface:
 
         # Adiciona o texto "Horas trabalhadas" acima do display
         self.texto_label = tk.Label(root, text="Horas Trabalhadas", font=("Helvetica", 14))
-        self.texto_label.pack(pady=5)
+        self.texto_label.grid(row=0, column=0, pady=5, columnspan=3)  # columnspan para estender sobre as 3 colunas
 
         self.total_horas_label = tk.Label(root, text="00:00:00", font=("Helvetica", 16), fg="green", bg="black")
-        self.total_horas_label.pack(pady=10)
+        self.total_horas_label.grid(row=1, column=0, pady=10, columnspan=3)  # columnspan para estender sobre as 3 colunas
 
         self.iniciar_button = tk.Button(root, text="Iniciar Trabalho", command=self.iniciar_trabalho)
-        self.iniciar_button.pack(pady=5)
+        self.iniciar_button.grid(row=2, column=1, pady=5, padx=5, columnspan=3)
 
         self.encerrar_button = tk.Button(root, text="Encerrar Trabalho", command=self.encerrar_trabalho, state=tk.DISABLED)
-        self.encerrar_button.pack(pady=5)
+        self.encerrar_button.grid(row=2, column=1, pady=5, padx=5, columnspan=3)
 
         self.pausa_10_button = tk.Button(root, text="Pausa 10 Min", command=lambda: self.registrar_pausa(10))
-        self.pausa_10_button.pack(side=tk.LEFT, padx=5)
+        self.pausa_10_button.grid(row=3, column=0, padx=5)
 
         self.pausa_20_button = tk.Button(root, text="Pausa 20 Min", command=lambda: self.registrar_pausa(20))
-        self.pausa_20_button.pack(side=tk.LEFT, padx=5)
+        self.pausa_20_button.grid(row=3, column=1, padx=5)
 
         self.pausa_30_button = tk.Button(root, text="Pausa 30 Min", command=lambda: self.registrar_pausa(30))
-        self.pausa_30_button.pack(side=tk.LEFT, padx=5)
+        self.pausa_30_button.grid(row=3, column=2, padx=5)
 
         self.atualizar_display()
 
@@ -47,4 +47,4 @@ class Interface:
 
     def atualizar_display(self):
         tempo_formatado = self.registro_horas.obter_tempo_formatado()
-        self.total_horas_label.config(text=f"Total de Horas: {tempo_formatado}")
+        self.total_horas_label.config(text=f"{tempo_formatado}")
