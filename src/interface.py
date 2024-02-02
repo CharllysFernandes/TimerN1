282b16daf65743c2ca833d97d45cd617e8ca3aca
Charllys Fernandes <charllys.daiany@gmail.com>
2024-02-01 23:29:25 -0300

    Add Buttons

--- src/interface.py
+++ src/interface.py
@@ -6,6 +6,7 @@ class Interface:
     def __init__(self, root, registro_horas):
         self.root = root
         self.registro_horas = registro_horas
+        self.is_trabalho_iniciado = False
 
         self.root.title("Folha de Ponto")
 
@@ -13,7 +14,7 @@ class Interface:
         self.texto_label = tk.Label(root, text="Folha de Ponto", font=("Helvetica", 16))
         self.texto_label.grid(row=0, column=0, pady=10, columnspan=4)  # columnspan para estender sobre as 4 colunas
 
-        self.iniciar_button = tk.Button(root, text="Iniciar", command=self.iniciar_trabalho)
+        self.iniciar_button = tk.Button(root, text="Iniciar", command=self.toggle_trabalho)
         self.iniciar_button.grid(row=1, column=0, pady=5, padx=5, columnspan=4)
 
         self.pausa_10_button = tk.Button(root, text="Iniciar Pausa 10", command=lambda: self.registrar_pausa(10))
@@ -46,3 +47,14 @@ class Interface:
         tempo_formatado = self.registro_horas.obter_tempo_formatado()
         self.texto_label.config(text=f"Folha de Ponto - {tempo_formatado}")
 
+    def toggle_trabalho(self):
+        if not self.is_trabalho_iniciado:
+            self.registro_horas.iniciar_trabalho()
+            self.is_trabalho_iniciado = True
+            self.iniciar_button.config(text="Encerrar")
+        else:
+            self.registro_horas.encerrar_trabalho()
+            self.is_trabalho_iniciado = False
+            self.iniciar_button.config(text="Iniciar")
+
+        self.atualizar_display()
